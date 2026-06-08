8731dc15e4341d08d7ddc2f8a24125942d5ac6e6
Hamdi Helmi <helmihamdi977@gmail.com>
2026-06-08 16:43:35 +0100

    app expo with nativewind

---
+++ mobile/components/Header.tsx
@@ -0,0 +1,73 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { HeaderProps } from "@/constants/types";
+import Ionicons from "@expo/vector-icons/build/Ionicons";
+import { COLORS } from "@/constants";
+import { useRouter } from "expo-router";
+
+export default function Header({
+  title,
+  showBack,
+  showSearch,
+  showMenu,
+  showLogo,
+  showCart,
+}: HeaderProps) {
+  const router = useRouter();
+  const { itemCount } = { itemCount: 3 };
+  return (
+    <View className="flex-row items-center justify-between px-4 py-3 bg-white">
+      {/* Gauche */}
+      <View className="flex-row items-center flex-1">
+        {showBack && (
+          <TouchableOpacity onPress={() => router.back()} className="mr-3">
+            <Ionicons name="arrow-back" size={24} color={COLORS.primary} />
+          </TouchableOpacity>
+        )}
+
+        {showMenu && (
+          <TouchableOpacity className="mr-3">
+            <Ionicons name="menu-outline" size={28} color={COLORS.primary} />
+          </TouchableOpacity>
+        )}
+
+        {showLogo ? (
+          <View className="flex-1">
+            <Image
+              source={require("@/assets/logo.png")}
+              style={{ width: "100%", height: 24 }}
+              resizeMode="contain"
+            />
+          </View>
+        ) : title ? (
+          <Text className="text-xl font-bold text-primary flex-1 text-center">
+            {title}
+          </Text>
+        ) : null}
+      </View>
+
+      {/* Droite */}
+      <View className="flex-row items-center gap-4">
+        {showSearch && (
+          <TouchableOpacity>
+            <Ionicons name="search-outline" size={24} color={COLORS.primary} />
+          </TouchableOpacity>
+        )}
+        {showCart && (
+          <TouchableOpacity onPress={() => router.push("/(tabs)/cart")}>
+            <View className="relative">
+              <Ionicons name="bag-outline" size={24} color={COLORS.primary} />
+              <View 
+               style={{ width: 16, height: 16, borderRadius: 8 }}
+              className="absolute -top-1 -right-1 bg-accent  rounded-full items-center justify-center ">
+                <Text className="text-white text-[10px] font-bold">
+                  {itemCount}
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+}
---
+++ mobile/components/ProductCard.tsx
@@ -0,0 +1,61 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+import { ProductCardProps } from "@/constants/types";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants";
+
+export default function ProductCard({ product }: ProductCardProps) {
+  const isLiked = false; // This should come from your state or props
+
+  return (
+    <Link href={`/product/${product._id}`} asChild>
+      <TouchableOpacity className="w-[48%] mb-4 rounded-lg overflow-hidden bg-white ">
+        <View className="relative w-full h-60 bg-gray-100">
+          <Image
+            source={{ uri: product.images[0] }}
+            className="w-full h-full"
+            resizeMode="cover"
+          />
+          <TouchableOpacity
+            className="absolute top-2 right-2 p-2 z-10 bg-white rounded-full 
+            shadow-sm"
+            onPress={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <Ionicons
+              name={isLiked ? "heart" : "heart-outline"}
+              size={20}
+              color={isLiked ? COLORS.accent : COLORS.primary}
+            />
+          </TouchableOpacity>
+          {product.isFeatured && (
+            <View className="absolute top-2 left-2 px-2 py-1 bg-black rounded">
+              <Text className="text-white text-xs font-bold uppercase">
+                Featured
+              </Text>
+            </View>
+          )}
+        </View>
+        <View className="p-3">
+          <View className="flex-row items-center mb-1">
+            <Ionicons name="star" size={14} color="#FFD700" />
+            <Text className="text-xs text-secondary ml-1">4.6</Text>
+          </View>
+          <Text
+            className="text-primary text-sm font-medium mb-1"
+            numberOfLines={1}
+          >
+            {product.name}
+          </Text>
+          <View className="flex-row items-center">
+            <Text className="text-primary text-base font-bold">
+              ${product.price.toFixed(2)}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Link>
+  );
+}
