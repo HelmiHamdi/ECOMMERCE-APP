aa145bed9e4e319c04820f2c3861726927f6e47b
Hamdi Helmi <helmihamdi977@gmail.com>
2026-06-16 16:14:32 +0100

    changed

--- mobile/constants/api.ts
+++ mobile/constants/api.ts
@@ -1,10 +1,10 @@
-/*import axios from "axios"
+import axios from "axios"
 
 const api = axios.create({baseURL: "https://shop-mobile-server.vercel.app/api"})
 
-export default api;*/
+export default api;
 
-import axios from "axios"
+/*import axios from "axios"
 import { Platform } from "react-native"
 
 const LOCAL_API_URL = Platform.select({
@@ -15,4 +15,4 @@ const LOCAL_API_URL = Platform.select({
 
 const api = axios.create({baseURL: LOCAL_API_URL})
 
-export default api;
+export default api;*/
